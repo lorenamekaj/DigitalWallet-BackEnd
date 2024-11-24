8f5a2734b0a1ffd9b8e22a389df17bf2e1cb9eba
Lorena Mekaj <lorenamekaj@gmail.com>
2024-11-24 10:24:31 +0100

    Springboot initialization

---
+++ digwallet/src/main/java/com/lorenamekaj/digwallet/DigwalletApplication.java
@@ -0,0 +1,13 @@
+package com.lorenamekaj.digwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigwalletApplication.class, args);
+	}
+
+}
---
+++ digwallet/src/test/java/com/lorenamekaj/digwallet/DigwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenamekaj.digwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
